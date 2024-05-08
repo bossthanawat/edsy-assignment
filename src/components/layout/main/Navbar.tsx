fe87ea953c025bf623d611a967f0b7305cd4ad09
bossthanawat <boss41680@gmail.com>
2024-05-08 18:01:18 +0700

    Update Navbar component with logo and styling

--- src/components/layout/main/Navbar.tsx
+++ src/components/layout/main/Navbar.tsx
@@ -1,5 +1,17 @@
+import { PATH_PAGE } from "@/router/path";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
-  return <>Navbar</>
-}
+  return (
+    <>
+      <div className="py-6 px-[88px]">
+        <NavLink to={PATH_PAGE.root}>
+          <p className="text-3xl font-bold bg-gradient-to-r from-[#0E34A0] to-[rgba(39, 93, 248, 0.8)] bg-clip-text text-transparent">
+            EDSY.
+          </p>
+        </NavLink>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
